9a13087d69778c643da362ecc8f32cb4314641d0
Ibaliqbal <iqbalmuthahhary@gmail.com>
2025-08-31 17:01:11 +0700

    simple example matter js

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".container");
+  const rect = container.getBoundingClientRect();
+  const { Engine, Body, Composite, Bodies, Runner, Render } = Matter;
+  const engine = Engine.create();
+  const runner = Runner.create();
+  const world = engine.world;
+
+  const boxA = Bodies.rectangle(200, 200, 100, 100);
+  const boxB = Bodies.rectangle(400, 200, 100, 100);
+  const boxC = Bodies.rectangle(600, 200, 100, 100);
+  
+  const ground = Bodies.rectangle(rect.width / 2, rect.height, rect.width, 1, {
+    isStatic: true,
+  });
+  const wallLeft = Bodies.rectangle(0, rect.height / 2, 1, rect.height, {
+    isStatic: true,
+  });
+
+  const wallRight = Bodies.rectangle(
+    rect.width,
+    rect.height / 2,
+    1,
+    rect.height,
+    {
+      isStatic: true,
+    }
+  );
+
+  const render = Render.create({
+    element: container,
+    engine: engine,
+    options: {
+      width: rect.width,
+      height: rect.height,
+      background: "#1c1c1c",
+      wireframes: false,
+    },
+  });
+
+  Composite.add(world, [boxA, boxB, boxC, ground, wallLeft, wallRight]);
+
+  Render.run(render);
+  Runner.run(runner, engine);
+});
