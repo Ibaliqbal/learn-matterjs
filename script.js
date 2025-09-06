f53de0f4c327a655ae86834d3f163dd7d4041f1d
Ibaliqbal <iqbalmuthahhary@gmail.com>
2025-09-06 23:55:15 +0700

    try new shape

--- script.js
+++ script.js
@@ -9,13 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const boxA = Bodies.rectangle(200, 200, 100, 100);
   const boxB = Bodies.rectangle(400, 200, 100, 100);
   const boxC = Bodies.rectangle(600, 200, 100, 100);
-  
+
   const ground = Bodies.rectangle(rect.width / 2, rect.height, rect.width, 1, {
     isStatic: true,
   });
   const wallLeft = Bodies.rectangle(0, rect.height / 2, 1, rect.height, {
     isStatic: true,
   });
+
+  const circleA = Bodies.circle(600, 100, 30);
 
   const wallRight = Bodies.rectangle(
     rect.width,
